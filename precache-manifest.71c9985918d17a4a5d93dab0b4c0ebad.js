a7b88f8c259836ca5f20f023905a56858c5c6fae
tabishadnan <tabishadnan000@gmail.com>
2020-12-23 02:59:31 +0500

    Updates

---
+++ precache-manifest.71c9985918d17a4a5d93dab0b4c0ebad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee2e2b4bbc11a291659fb6270b2081c",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "9c3bdf54d22447b91504",
+    "url": "/react-todo-list/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "cecb89db3e292016bc1f",
+    "url": "/react-todo-list/static/css/main.38a9eec7.chunk.css"
+  },
+  {
+    "revision": "9c3bdf54d22447b91504",
+    "url": "/react-todo-list/static/js/2.75e6cb29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.75e6cb29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cecb89db3e292016bc1f",
+    "url": "/react-todo-list/static/js/main.58874b17.chunk.js"
+  },
+  {
+    "revision": "2c6e5d8f9eaaedf40455",
+    "url": "/react-todo-list/static/js/runtime-main.f23babde.js"
+  }
+]);
